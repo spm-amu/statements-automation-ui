7f3954688d2c28a91c34370da377f57a36ac9feb
ashandla <ashandla@gmail.com>
2022-10-30 01:30:55 +0200

    Scrolling for settings

--- src/main/common/components/view/People.jsx
+++ src/main/common/components/view/People.jsx
@@ -1,12 +1,12 @@
-import React, { useEffect, useState } from 'react';
+import React, {useEffect, useState} from 'react';
 import SearchBar from "../SearchBar";
 import './People.css'
 import PersonCard from "../PersonCard";
-import { host, post } from '../../service/RestService';
+import {host, post} from '../../service/RestService';
 import socketManager from '../../service/SocketManager';
-import { MessageType } from '../../types';
+import {MessageType} from '../../types';
 import uuid from 'react-uuid';
-import { useNavigate } from 'react-router-dom';
+import {useNavigate} from 'react-router-dom';
 import Utils from "../../Utils";
 
 const People = (props) => {
@@ -19,48 +19,55 @@ const People = (props) => {
   const {meetingId, exclusions} = props;
 
   useEffect(() => {
-    if(meetingId) {
-      // TODO : If a meeting id is passed, search for all meeting attendees
+    if (meetingId) {
+      search(null, true);
     }
   }, []);
 
-  const onValueChangedHandler = (searchValue) => {
-    if(meetingId) {
-      // TODO : If a meeting id is passed, search for all meeting attendees and pass the search value as a filter ONLY within those attendees
-    } else {
-      // NB : If there are exclusions(i.e people who have already joined the meeting), we call the API and pass the exclusions. We do not want to include them in the search result
-      // @Nsovo - update the backend to take into account any exclusions
-      if (searchValue) {
-        post(`${host}/api/v1/auth/search`, (response) => {
-            console.log('RESPONSE: ', response);
-            setSearchResult(response);
-          }, (e) => {
+  const search = (searchValue, searchNullValue = false) => {
+    // NB : If there are exclusions(i.e people who have already joined the meeting), we call the API and pass the exclusions. We do not want to include them in the search result
+    // @Nsovo - update the backend to take into account any exclusions
+    // NB : If a meeting id is passed, search for all meeting attendees and pass the search value as a filter ONLY within those attendees
+    // @Nsovo - update the backend to take into account any meeting attendees
 
-          },
-          {
-            "parameters": [
-              {
-                "name": 'emailAddress',
-                "value": searchValue
-              },
-              {
-                "name": 'exclusions',
-                "value": exclusions ? exclusions : null
-              }
-            ],
-            "pageSize": 2000,
-            "currentPage": 0
-          })
-      } else {
-        setSearchResult({
-          records: []
-        });
-      }
+    if (searchValue || searchNullValue) {
+      post(`${host}/api/v1/auth/search`, (response) => {
+          console.log('RESPONSE: ', response);
+          setSearchResult(response);
+        }, (e) => {
+
+        },
+        {
+          "parameters": [
+            {
+              "name": 'emailAddress',
+              "value": searchValue
+            },
+            {
+              "name": 'exclusions',
+              "value": exclusions ? exclusions : null
+            },
+            {
+              "name": 'meetingId',
+              "value": meetingId ? meetingId : null
+            }
+          ],
+          "pageSize": 2000,
+          "currentPage": 0
+        })
+    } else {
+      setSearchResult({
+        records: []
+      });
     }
   };
 
-  const onAudioCallHandler = ( userToCall ) => {
-    if(props.onAudioCallHandler) {
+  const onValueChangedHandler = (searchValue) => {
+    search(searchValue);
+  };
+
+  const onAudioCallHandler = (userToCall) => {
+    if (props.onAudioCallHandler) {
       props.onAudioCallHandler(userToCall);
     } else {
       let userDetails = JSON.parse(sessionStorage.getItem('userDetails'));
@@ -91,18 +98,20 @@ const People = (props) => {
   return (
     <div className={'w-100 h-100 people-container'}>
       <div className={'search'}>
-        <SearchBar valueChangeHandler={onValueChangedHandler} onSearch={(searchValue) => {}}/>
+        <SearchBar valueChangeHandler={onValueChangedHandler} onSearch={(searchValue) => {
+        }}/>
       </div>
       <div className={'people-content row'}>
         {searchResult.records.map((user, index) => {
-          return <div key={index} className={'col person-card-wrapper'} style={{marginLeft: '0', paddingLeft: '0', maxWidth: '400px'}}>
-              <PersonCard onAudioCallHandler={(data) => onAudioCallHandler(data)}
-                          data={user}
-                          avatarSize={!Utils.isNull(props.avatarSize) ? props.avatarSize : true}
-                          showOnlineIndicator={!Utils.isNull(props.showOnlineIndicator) ? props.showOnlineIndicator : true}
-                          chatEnabled={!Utils.isNull(props.chatEnabled) ? props.chatEnabled : true}
-                          dialEnabled={!Utils.isNull(props.dialEnabled) ? props.dialEnabled : true}
-              />
+          return <div key={index} className={'col person-card-wrapper'}
+                      style={{marginLeft: '0', paddingLeft: '0', maxWidth: '400px'}}>
+            <PersonCard onAudioCallHandler={(data) => onAudioCallHandler(data)}
+                        data={user}
+                        avatarSize={!Utils.isNull(props.avatarSize) ? props.avatarSize : true}
+                        showOnlineIndicator={!Utils.isNull(props.showOnlineIndicator) ? props.showOnlineIndicator : true}
+                        chatEnabled={!Utils.isNull(props.chatEnabled) ? props.chatEnabled : true}
+                        dialEnabled={!Utils.isNull(props.dialEnabled) ? props.dialEnabled : true}
+            />
           </div>
         })}
       </div>
