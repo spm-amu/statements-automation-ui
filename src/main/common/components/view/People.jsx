4c951aab92b3f2bdf1666db46fc6eb235db5c470
ashandla <ashandla@gmail.com>
2023-02-11 23:13:04 +0200

    UAT Prep

--- src/main/common/components/view/People.jsx
+++ src/main/common/components/view/People.jsx
@@ -21,7 +21,7 @@ const People = (props) => {
 
   useEffect(() => {
     //if (meetingId) {
-      search(null, true);
+    search(null, true);
     //}
   }, []);
 
@@ -31,36 +31,30 @@ const People = (props) => {
     // NB : If a meeting id is passed, search for all meeting attendees and pass the search value as a filter ONLY within those attendees
     // @Nsovo - update the backend to take into account any meeting attendees
 
-    if (searchValue || searchNullValue) {
-      post(`${host}/api/v1/auth/search`, (response) => {
-          console.log('RESPONSE: ', response);
-          setSearchResult(response);
-        }, (e) => {
+    post(`${host}/api/v1/auth/search`, (response) => {
+        console.log('RESPONSE: ', response);
+        setSearchResult(response);
+      }, (e) => {
 
-        },
-        {
-          "parameters": [
-            {
-              "name": 'emailAddress',
-              "value": searchValue
-            },
-            {
-              "name": 'exclusions',
-              "value": exclusions ? exclusions : null
-            },
-            {
-              "name": 'meetingId',
-              "value": meetingId ? meetingId : null
-            }
-          ],
-          "pageSize": 2000,
-          "currentPage": 0
-        })
-    } else {
-      setSearchResult({
-        records: []
-      });
-    }
+      },
+      {
+        "parameters": [
+          {
+            "name": 'emailAddress',
+            "value": searchValue
+          },
+          {
+            "name": 'exclusions',
+            "value": exclusions ? exclusions : null
+          },
+          {
+            "name": 'meetingId',
+            "value": meetingId ? meetingId : null
+          }
+        ],
+        "pageSize": 2000,
+        "currentPage": 0
+      })
   };
 
   const onValueChangedHandler = (searchValue) => {
@@ -106,7 +100,13 @@ const People = (props) => {
       <div className={'people-content row'} style={{maxHeight: props.meetingId ? '50%' : '77vh'}}>
         {searchResult.records.map((user, index) => {
           return <div key={index} className={'col person-card-wrapper'}
-                      style={{marginLeft: '0', paddingLeft: '0', minWidth: '320px', maxWidth: '320px', marginBottom: '8px'}}>
+                      style={{
+                        marginLeft: '0',
+                        paddingLeft: '0',
+                        minWidth: '320px',
+                        maxWidth: '320px',
+                        marginBottom: '8px'
+                      }}>
             <PersonCard onAudioCallHandler={(data) => onAudioCallHandler(data)}
                         data={user}
                         avatarSize={!Utils.isNull(props.avatarSize) ? props.avatarSize : true}
