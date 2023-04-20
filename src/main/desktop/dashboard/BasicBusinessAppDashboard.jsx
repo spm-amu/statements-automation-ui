7471ca25c0c78bbf9b5728e77ce0f9d9a37ce061
Nsovo Ngobeni <nngobz@gmail.com>
2023-04-20 07:34:05 +0200

    Email template

--- src/main/desktop/dashboard/BasicBusinessAppDashboard.jsx
+++ src/main/desktop/dashboard/BasicBusinessAppDashboard.jsx
@@ -557,7 +557,7 @@ const BasicBusinessAppDashboard = (props) => {
       loadHost("http://localhost:8080/vc", "http:"http://ARMHOVC01/vc", "http://ARMHOVC01", false//localhost:8000");
     }*/
 
-    loadHost("http://localhost:8080/vc", "https://svn.agilemotion.co.za", false);
+    loadHost("http://ARMHOVC01/vc", "http://ARMHOVC01", false);
   }, []);
 
   React.useEffect(() => {
--- src/main/menu.ts
+++ src/main/menu.ts
@@ -57,21 +57,6 @@ export default class MenuBuilder {
       label: 'Armscor Connect',
       submenu: [
         {
-          label: 'About Armscor Connect',
-          selector: '',
-        },
-        { type: 'separator' },
-        {
-          label: 'Preferences',
-          selector: '',
-        },
-        { type: 'separator' },
-        {
-          label: 'Services',
-          submenu: [],
-        },
-        { type: 'separator' },
-        {
           label: 'Hide Armscor Connect',
           accelerator: 'Command+H',
           selector: 'hide:',
@@ -92,112 +77,8 @@ export default class MenuBuilder {
         },
       ],
     };
-    const subMenuEdit: DarwinMenuItemConstructorOptions = {
-      label: 'Edit',
-      submenu: [
-        { label: 'Undo', accelerator: 'Command+Z', selector: 'undo:' },
-        { label: 'Redo', accelerator: 'Shift+Command+Z', selector: 'redo:' },
-        { type: 'separator' },
-        { label: 'Cut', accelerator: 'Command+X', selector: 'cut:' },
-        { label: 'Copy', accelerator: 'Command+C', selector: 'copy:' },
-        { label: 'Paste', accelerator: 'Command+V', selector: 'paste:' },
-        {
-          label: 'Select All',
-          accelerator: 'Command+A',
-          selector: 'selectAll:',
-        },
-      ],
-    };
-    const subMenuViewDev: MenuItemConstructorOptions = {
-      label: 'View',
-      submenu: [
-        {
-          label: 'Reload',
-          accelerator: 'Command+R',
-          click: () => {
-            this.mainWindow.webContents.reload();
-          },
-        },
-        {
-          label: 'Toggle Full Screen',
-          accelerator: 'Ctrl+Command+F',
-          click: () => {
-            this.mainWindow.setFullScreen(!this.mainWindow.isFullScreen());
-          },
-        },
-        {
-          label: 'Toggle Developer Tools',
-          accelerator: 'Alt+Command+I',
-          click: () => {
-            this.mainWindow.webContents.toggleDevTools();
-          },
-        },
-      ],
-    };
-    const subMenuViewProd: MenuItemConstructorOptions = {
-      label: 'View',
-      submenu: [
-        {
-          label: 'Toggle Full Screen',
-          accelerator: 'Ctrl+Command+F',
-          click: () => {
-            this.mainWindow.setFullScreen(!this.mainWindow.isFullScreen());
-          },
-        },
-      ],
-    };
-    const subMenuWindow: DarwinMenuItemConstructorOptions = {
-      label: 'Window',
-      submenu: [
-        {
-          label: 'Minimize',
-          accelerator: 'Command+M',
-          selector: 'performMiniaturize:',
-        },
-        { label: 'Close', accelerator: 'Command+W', selector: 'performClose:' },
-        { type: 'separator' },
-        { label: 'Bring All to Front', selector: 'arrangeInFront:' },
-      ],
-    };
-    const subMenuHelp: MenuItemConstructorOptions = {
-      label: 'Help',
-      submenu: [
-        {
-          label: 'Learn More',
-          click() {
-            shell.openExternal('https://www.armscor.co.za');
-          },
-        },
-        {
-          label: 'Documentation',
-          click() {
-            shell.openExternal(
-              'https://www.armscor.co.za'
-            );
-          },
-        },
-        {
-          label: 'Privacy Policy',
-          click() {
-            shell.openExternal('https://www.armscor.co.za');
-          },
-        },
-        {
-          label: 'Armscor Connect Help',
-          click() {
-            shell.openExternal('https://www.armscor.co.za');
-          },
-        },
-      ],
-    };
 
-    const subMenuView =
-      process.env.NODE_ENV === 'development' ||
-      process.env.DEBUG_PROD === 'true'
-        ? subMenuViewDev
-        : subMenuViewProd;
-
-    return [subMenuAbout, subMenuEdit, subMenuView, subMenuWindow, subMenuHelp];
+    return [subMenuAbout];
   }
 
   buildDefaultTemplate() {
