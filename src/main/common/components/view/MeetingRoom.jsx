127557bfc9c86edaafda889944acfbe033d8d987
ashandla <ashandla@gmail.com>
2023-04-27 00:12:21 +0200

    UAT fixes

--- src/main/common/components/view/MeetingRoom.jsx
+++ src/main/common/components/view/MeetingRoom.jsx
@@ -333,18 +333,16 @@ const MeetingRoom = (props) => {
       const newParticipants = participants.filter((p) => p.userId !== userId);
 
       if (newParticipants.length === 0 && isDirectCall) {
-        setStep(Steps.SESSION_ENDED);
-        appManager.remove('CURRENT_MEETING');
-        //onCallEnded();
-        //props.closeHandler();
+        //setStep(Steps.SESSION_ENDED);
+        onCallEnded();
+        props.closeHandler();
       } else {
         setParticipants(newParticipants);
         if (newParticipants.length === 0) {
-          //onCallEnded();
-          //props.closeHandler();
-          setAllUserParticipantsLeft(true);
-          setStep(Steps.SESSION_ENDED);
-          appManager.remove('CURRENT_MEETING');
+          onCallEnded();
+          props.closeHandler();
+          //setAllUserParticipantsLeft(true);
+          //setStep(Steps.SESSION_ENDED);
 
           get(
             `${appManager.getAPIHost()}/api/v1/meeting/end/${selectedMeeting.id}`,
@@ -1076,11 +1074,12 @@ const MeetingRoom = (props) => {
       props.onEndCall();
       props.closeHandler();
     } else {
-      setSomeoneSharing(false);
+      /*setSomeoneSharing(false);
       setMeetingParticipantGridMode("DEFAULT");
       setStep(Steps.SESSION_ENDED);
-      setScreenShared(false);
-      appManager.remove('CURRENT_MEETING');
+      setScreenShared(false);*/
+      onCallEnded();
+      props.closeHandler();
     }
   };
 
