35a89141cd8369d9deae353bf9b4a9e6bdd7907f
Nsovo Ngobeni <nngobz@gmail.com>
2023-06-06 07:03:29 +0200

    Request to call event

--- src/main/main.ts
+++ src/main/main.ts
@@ -18,6 +18,53 @@ import Store from "electron-store";
 
 const store = new Store();
 
+autoUpdater.requestHeaders = { "PRIVATE-TOKEN": "glpat-Kzd2MSm3QeiveSof5KT1" };
+autoUpdater.autoDownload = true;
+
+autoUpdater.setFeedURL({
+  provider: "generic",
+  url: "https://gitlab.com/38595734/-/jobs/artifacts/master/raw/dist?job=build"
+});
+
+autoUpdater.on('checking-for-update', function () {
+  sendStatusToWindow('Checking for update...');
+});
+
+autoUpdater.on('update-available', function (info) {
+  sendStatusToWindow('Update available.');
+});
+
+autoUpdater.on('update-not-available', function (info) {
+  sendStatusToWindow('Update not available.');
+});
+
+autoUpdater.on('error', function (err) {
+  sendStatusToWindow('Error in auto-updater.');
+});
+
+autoUpdater.on('download-progress', function (progressObj) {
+  let log_message = "Download speed: " + progressObj.bytesPerSecond;
+  log_message = log_message + ' - Downloaded ' + parseInt(progressObj.percent) + '%';
+  log_message = log_message + ' (' + progressObj.transferred + "/" + progressObj.total + ')';
+  sendStatusToWindow(log_message);
+});
+
+autoUpdater.on('update-downloaded', function (info) {
+  sendStatusToWindow('Update downloaded; will install in 1 seconds');
+});
+
+autoUpdater.on('update-downloaded', function (info) {
+  setTimeout(function () {
+    autoUpdater.quitAndInstall();
+  }, 1000);
+});
+
+autoUpdater.checkForUpdates();
+
+function sendStatusToWindow(message) {
+  console.log(message);
+}
+
 class AppUpdater {
   constructor() {
     log.transports.file.resolvePath = () => path.join('/Users/nsovongobeni/Work/Armscore/ui', '/logsmain.log');
