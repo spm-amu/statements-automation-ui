bf75314c44e27ef4e7d8b95df306733b86755a7b
ashandla <ashandla@gmail.com>
2023-07-16 01:17:26 +0200

    Refactor for introducing mediasoup

--- src/main/desktop/dashboard/BasicBusinessAppDashboard.jsx
+++ src/main/desktop/dashboard/BasicBusinessAppDashboard.jsx
@@ -713,6 +713,7 @@ const BasicBusinessAppDashboard = (props) => {
     }*/
 
     loadHost("https://svn.agilemotion.co.za/vc", "https://svn.agilemotion.co.za", true);
+    loadHost("http://localhost:8080/vc", "https://svn.agilemotion.co.za", true);
     //loadHost("http://ARMHOVC01/vc", "https://ARMHOVC01:38080", true);
   }, []);
 
